7d7bbd386ed471f0ed8e2d5d17e5e03d7e8c57b8
Kasperi Aavasaari <kjaava@utu.fi>
2020-03-27 18:30:50 +0200

    Add basic block models

--- src/Board.java
+++ src/Board.java
@@ -1,28 +1,52 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 
 public class Board extends JPanel implements KeyListener{
 	
 	private Shape currentShape;
+	private final int blockSize = 30;
+	private int[][] blocks;
 	
 	public Board() {
 		this.addKeyListener(this);
 		this.setFocusable(true);
 		this.requestFocus();
-		this.currentShape = new Shape(0,0);
+		this.currentShape = new Shape(7);
+		this.blocks = new int[20][10];
+		this.setBlocks(this.currentShape.getCoords());
 	}
 	
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.setColor(Color.black);
-		g.fillRect(this.currentShape.getX(), this.currentShape.getY(), 30, 30);
+		g.setColor(Color.BLUE);
+		for (int i = 0; i < this.blocks.length; i++) {
+			for (int j = 0; j < this.blocks[i].length; j++) {
+				if (this.blocks[i][j] != 0) {
+					g.fillRect(j * this.blockSize, i * this.blockSize, this.blockSize - 1, this.blockSize - 1);
+				}
+			}
+		}
 		
+	}
+	
+	//for debugging
+	private void printBlocks() {
+		for (int i = 0; i < this.blocks.length; i++) {
+			System.out.print("[ ");
+			for(int j = 0; j < this.blocks[i].length; j++) {
+				System.out.print(this.blocks[i][j] + " ");
+			}
+			System.out.println("]");
+		}
 	}
 	
 	@Override 
@@ -31,35 +55,41 @@ public class Board extends JPanel implements KeyListener{
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
-		
-	}
-
-
-	@Override
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		
 		switch(key) {
 		case  KeyEvent.VK_LEFT:
-			this.currentShape.setX(this.currentShape.getX() - 30);
-			System.out.println(this.currentShape.getX());
-			System.out.println("testi_vk_left");
+			//this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 0;
+			//this.currentShape.setX(this.currentShape.getX() - 1);
+			//this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 1;
+			//System.out.println(this.currentShape.getX());
+			//System.out.println("testi_vk_left");
+			printBlocks();
 			break;
 		case KeyEvent.VK_RIGHT:
-			this.currentShape.setX(this.currentShape.getX() + 30);
+			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 0;
+			this.currentShape.setX(this.currentShape.getX() + 1);
+			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 1;
 			System.out.println("testi_vk_right");
 			System.out.println(this.currentShape.getX());
+			printBlocks();
 			break;
 		case KeyEvent.VK_DOWN:
-			this.currentShape.setY(this.currentShape.getY() + 30);
+			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 0;
+			this.currentShape.setY(this.currentShape.getY() + 1);
+			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 1;
 			System.out.println("testi_vk_down");
 			System.out.println(this.currentShape.getY());
+			printBlocks();
 			break;
 		case KeyEvent.VK_UP:
-			this.currentShape.setY(this.currentShape.getY() - 30);
+			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 0;
+			this.currentShape.setY(this.currentShape.getY() - 1);
+			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 1;
 			System.out.println("testi_vk_up");
 			System.out.println(this.currentShape.getY());
+			printBlocks();
 			break;
 			
 		default:
@@ -70,12 +100,28 @@ public class Board extends JPanel implements KeyListener{
 		
 		
 	}
+	
+	private void setBlocks(ArrayList<Point> blockCoords) {
+		for (Point p : blockCoords) {
+			this.blocks[(int) p.getY() + this.currentShape.getY()][(int) p.getX() + this.currentShape.getX()] = this.currentShape.getShape();
+			}
+		}
 
+	private void setToZero(ArrayList<Integer> coords) {
+		
+	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
 
 }
--- src/Shape.java
+++ src/Shape.java
@@ -1,13 +1,69 @@
+import java.awt.Point;
+import java.util.ArrayList;
 
 public class Shape {
 	
+	private int shape;
 	private int x;
 	private int y;
+	private ArrayList<Point> coords;
 	
-	public Shape(int x, int y) {
-		this.x = x;
-		this.y = y;
-;
+	// shape defines which shape the shape is, x is the main x-coordinate and y is the main y-coordinate
+	public Shape(int shape) {
+		this.shape = shape;
+		this.x = 4;
+		this.y = 1;
+		this.coords = new ArrayList<>();
+		this.setCoords(this.shape);
+	}
+	
+	private void setCoords(int shape) {
+		switch (shape) {
+		case 1:
+			this.coords.add(new Point(0,0));
+			this.coords.add(new Point(-1,0));
+			this.coords.add(new Point(1,0));
+			this.coords.add(new Point(2,0));
+			break;
+		case 2:
+			this.coords.add(new Point(0,0));
+			this.coords.add(new Point(1,0));
+			this.coords.add(new Point(0,1));
+			this.coords.add(new Point(1,1));
+			break;
+		case 3:
+			this.coords.add(new Point(-1,0));
+			this.coords.add(new Point(0,0));
+			this.coords.add(new Point(1,0));
+			this.coords.add(new Point(1,1));
+			break;
+		case 4:
+			this.coords.add(new Point(-1,1));
+			this.coords.add(new Point(-1,0));
+			this.coords.add(new Point(0,0));
+			this.coords.add(new Point(1,0));
+			break;
+		case 5:
+			this.coords.add(new Point(-1,1));
+			this.coords.add(new Point(0,1));
+			this.coords.add(new Point(0,0));
+			this.coords.add(new Point(1,0));
+			break;
+		case 6:
+			this.coords.add(new Point(-1,0));
+			this.coords.add(new Point(0,0));
+			this.coords.add(new Point(0,1));
+			this.coords.add(new Point(1,0));
+			break;
+		case 7:
+			this.coords.add(new Point(-1,0));
+			this.coords.add(new Point(0,0));
+			this.coords.add(new Point(0,1));
+			this.coords.add(new Point(1,1));
+			break;
+		default:
+			break;
+		}
 	}
 	
 	public int getX() {
@@ -18,6 +74,14 @@ public class Shape {
 		return this.y;
 	}
 	
+	public int getShape() {
+		return this.shape;
+	}
+	
+	public ArrayList<Point> getCoords() {
+		return this.coords;
+	}
+	
 	public void setX(int x) {
 		this.x = x;
 	}
@@ -25,5 +89,7 @@ public class Shape {
 	public void setY(int y) {
 		this.y = y;
 	}
+	
+	
 
 }
