83b20109cdef4e49250f63b13832551935aa14bc
Kasperi Aavasaari <kjaava@utu.fi>
2020-03-31 20:01:04 +0300

    Add moving blocks down, block rotation and illegal block movement tests

--- src/Board.java
+++ src/Board.java
@@ -18,24 +18,52 @@ public class Board extends JPanel implements KeyListener{
 		this.addKeyListener(this);
 		this.setFocusable(true);
 		this.requestFocus();
-		this.currentShape = new Shape(7);
+		this.currentShape = new Shape(5);
 		this.blocks = new int[20][10];
-		this.setBlocks(this.currentShape.getCoords());
+		this.setBlocks(this.currentShape.getCoords(this.currentShape.getRotation()));
+		this.blocks[0][0] = 1;
+		this.blocks[0][1] = 2;
+		this.blocks[0][2] = 3;
+		this.blocks[0][3] = 4;
+		this.blocks[0][4] = 5;
+		this.blocks[0][5] = 6;
+		this.blocks[0][6] = 7;
 	}
 	
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.setColor(Color.BLUE);
 		for (int i = 0; i < this.blocks.length; i++) {
 			for (int j = 0; j < this.blocks[i].length; j++) {
 				if (this.blocks[i][j] != 0) {
+					g.setColor(this.getColor(this.blocks[i][j]));
 					g.fillRect(j * this.blockSize, i * this.blockSize, this.blockSize - 1, this.blockSize - 1);
 				}
 			}
 		}
 		
+	}
+	
+	private Color getColor(int number) {
+		switch (number) {
+		case 1:
+			return new Color(51, 204, 255); //light blue
+		case 2:
+			return new Color(255,255,0); //yellow
+		case 3:
+			return new Color(0,0,204); //dark blue
+		case 4:
+			return new Color(255,153,0); //orange
+		case 5:
+			return new Color(0,255,51); //light green
+		case 6:
+			return new Color(255,0,0); //red
+		case 7:
+			return new Color(102,0,153); //purple
+		default:
+			return new Color(255,255,255); //white
+		}
 	}
 	
 	//for debugging
@@ -47,6 +75,7 @@ public class Board extends JPanel implements KeyListener{
 			}
 			System.out.println("]");
 		}
+		System.out.println();
 	}
 	
 	@Override 
@@ -60,36 +89,20 @@ public class Board extends JPanel implements KeyListener{
 		
 		switch(key) {
 		case  KeyEvent.VK_LEFT:
-			//this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 0;
-			//this.currentShape.setX(this.currentShape.getX() - 1);
-			//this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 1;
-			//System.out.println(this.currentShape.getX());
-			//System.out.println("testi_vk_left");
+			this.moveToSide(-1);
 			printBlocks();
 			break;
 		case KeyEvent.VK_RIGHT:
-			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 0;
-			this.currentShape.setX(this.currentShape.getX() + 1);
-			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 1;
-			System.out.println("testi_vk_right");
-			System.out.println(this.currentShape.getX());
+			this.moveToSide(1);
 			printBlocks();
 			break;
 		case KeyEvent.VK_DOWN:
-			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 0;
-			this.currentShape.setY(this.currentShape.getY() + 1);
-			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 1;
-			System.out.println("testi_vk_down");
-			System.out.println(this.currentShape.getY());
+			this.moveDown();
 			printBlocks();
 			break;
 		case KeyEvent.VK_UP:
-			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 0;
-			this.currentShape.setY(this.currentShape.getY() - 1);
-			this.blocks[this.currentShape.getY()][this.currentShape.getX()] = 1;
-			System.out.println("testi_vk_up");
-			System.out.println(this.currentShape.getY());
-			printBlocks();
+			this.rotateBlock();
+			//printBlocks();
 			break;
 			
 		default:
@@ -100,6 +113,116 @@ public class Board extends JPanel implements KeyListener{
 		
 		
 	}
+	private ArrayList<Point> getTestCoordinates(ArrayList<Point> blockCoords, int direction, boolean rotation) {
+		ArrayList<Point> oldCoords = new ArrayList<>();
+		System.out.println("old");
+		for(Point p : blockCoords) {
+			System.out.println("(" + ((int) p.getY() + this.currentShape.getY()) + " , " + ((int) p.getX() + this.currentShape.getX()) + ")");
+			oldCoords.add(new Point(((int) p.getX() + this.currentShape.getX()), ((int) p.getY() + this.currentShape.getY())));
+		}
+		ArrayList<Point> newCoords = new ArrayList<>();
+		System.out.println("new");
+		if (rotation) {
+			ArrayList<Point> rotatedCoords = this.currentShape.getCoords(this.currentShape.nextRotation());
+			for(Point p : rotatedCoords) {
+				newCoords.add(new Point((int) p.getX() + this.currentShape.getX(), (int) p.getY() + this.currentShape.getY()));
+				System.out.println("(" + ((int) p.getX()) + " , " + (int) p.getY() + ")");
+			}
+		}else if(direction == 0) {
+			for(Point p : blockCoords) {
+				newCoords.add(new Point(((int) p.getX() + this.currentShape.getX()), ((int) p.getY() + this.currentShape.getY() + 1)));
+				System.out.println("(" + ((int) p.getY() + this.currentShape.getY() + 1) + " , " + ((int) p.getX() + this.currentShape.getX()) + ")");
+			}
+		} else {
+			for(Point p : blockCoords) {
+				System.out.println("(" + ((int) p.getY() + this.currentShape.getY()) + " , " + ((int) p.getX() + this.currentShape.getX() + direction) + ")");
+				newCoords.add(new Point(((int) p.getX() + this.currentShape.getX() + direction), ((int) p.getY() + this.currentShape.getY())));
+			}
+		}
+		
+		System.out.println("check");
+		ArrayList<Point> testCoords = new ArrayList<>();
+		for(Point np : newCoords) {
+			boolean contains = false;
+			for(Point op : oldCoords) {
+				if(op.getX() == np.getX() && op.getY() == np.getY()) {
+					contains = true;
+				}
+			}
+			if (!contains) {
+				testCoords.add(new Point((int) np.getX(), (int) np.getY()));
+			}
+		}
+		for(Point p : testCoords) {
+			System.out.println("(" + ((int) p.getY()) + " , " + ((int) p.getX()) + ")");
+		}
+		
+		return testCoords;
+	}
+
+	
+	private void moveToSide(int direction) {
+		ArrayList<Point> testCoords = this.getTestCoordinates(this.currentShape.getCoords(this.currentShape.getRotation()), direction, false);
+		
+		for (Point p : testCoords) {
+			int x = (int) p.getX();
+			int y = (int) p.getY();
+			if (x > 9 || x < 0) {
+				return;
+			}
+			
+			if (this.blocks[y][x] != 0) {
+				return;
+			}
+		}
+
+		this.setToZero(this.currentShape.getCoords(this.currentShape.getRotation()));
+		this.currentShape.setX(this.currentShape.getX() + direction);
+		this.setBlocks(this.currentShape.getCoords(this.currentShape.getRotation()));
+	}
+	
+	private void moveDown() {
+		ArrayList<Point> testCoords = this.getTestCoordinates(this.currentShape.getCoords(this.currentShape.getRotation()), 0, false);
+		
+		for (Point p : testCoords) {
+			int x = (int) p.getX();
+			int y = (int) p.getY();
+			if (y > 19) {
+				return;
+			}
+			
+			if (this.blocks[y][x] != 0) {
+				return;
+			}
+		}
+		
+		this.setToZero(this.currentShape.getCoords(this.currentShape.getRotation()));
+		this.currentShape.setY(this.currentShape.getY() + 1);
+		this.setBlocks(this.currentShape.getCoords(this.currentShape.getRotation()));
+	}
+	
+	private void rotateBlock() {
+		ArrayList<Point> testCoords = this.getTestCoordinates(this.currentShape.getCoords(this.currentShape.getRotation()), 0, true);
+		for (Point p : testCoords) {
+			int x = (int) p.getX();
+			int y = (int) p.getY();
+			
+			if (y > 19 || y < 0) {
+				return;
+			}
+			
+			if (x > 9 || x < 0) {
+				return;
+			}
+			
+			if (this.blocks[y][x] != 0) {
+				return;
+			}
+		}
+		this.setToZero(this.currentShape.getCoords(this.currentShape.getRotation()));
+		this.currentShape.changeRotation();
+		this.setBlocks(this.currentShape.getCoords(this.currentShape.getRotation()));
+	}
 	
 	private void setBlocks(ArrayList<Point> blockCoords) {
 		for (Point p : blockCoords) {
@@ -107,20 +230,19 @@ public class Board extends JPanel implements KeyListener{
 			}
 		}
 
-	private void setToZero(ArrayList<Integer> coords) {
-		
+	private void setToZero(ArrayList<Point> blockCoords) {
+		for (Point p : blockCoords) {
+			this.blocks[(int) p.getY() + this.currentShape.getY()][(int) p.getX() + this.currentShape.getX()] = 0;
+		}
 	}
-
+	
 	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void keyReleased(KeyEvent e) {		
 	}
 	
 
 	@Override
-	public void keyTyped(KeyEvent e) {
-		
+	public void keyTyped(KeyEvent e) {		
 	}
 
 
--- src/Shape.java
+++ src/Shape.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Point;
 import java.util.ArrayList;
 
@@ -6,63 +7,148 @@ public class Shape {
 	private int shape;
 	private int x;
 	private int y;
-	private ArrayList<Point> coords;
+	private ArrayList<ArrayList<Point>> coords;
+	private int rotation;
 	
 	// shape defines which shape the shape is, x is the main x-coordinate and y is the main y-coordinate
 	public Shape(int shape) {
 		this.shape = shape;
 		this.x = 4;
 		this.y = 1;
-		this.coords = new ArrayList<>();
-		this.setCoords(this.shape);
+		this.coords = new ArrayList<ArrayList<Point>>();
+		this.initShape(this.shape);
+		this.rotation = 0;
 	}
 	
-	private void setCoords(int shape) {
+	private void initShape(int shape) {
 		switch (shape) {
 		case 1:
-			this.coords.add(new Point(0,0));
-			this.coords.add(new Point(-1,0));
-			this.coords.add(new Point(1,0));
-			this.coords.add(new Point(2,0));
+			this.initCoords(2);
+			this.coords.get(0).add(new Point(0,0));
+			this.coords.get(0).add(new Point(-1,0));
+			this.coords.get(0).add(new Point(1,0));
+			this.coords.get(0).add(new Point(2,0));
+			
+			this.coords.get(1).add(new Point(0,-1));
+			this.coords.get(1).add(new Point(0,0));
+			this.coords.get(1).add(new Point(0,1));
+			this.coords.get(1).add(new Point(0,2));
 			break;
 		case 2:
-			this.coords.add(new Point(0,0));
-			this.coords.add(new Point(1,0));
-			this.coords.add(new Point(0,1));
-			this.coords.add(new Point(1,1));
+			this.initCoords(1);
+			this.coords.get(0).add(new Point(0,0));
+			this.coords.get(0).add(new Point(1,0));
+			this.coords.get(0).add(new Point(0,1));
+			this.coords.get(0).add(new Point(1,1));
 			break;
 		case 3:
-			this.coords.add(new Point(-1,0));
-			this.coords.add(new Point(0,0));
-			this.coords.add(new Point(1,0));
-			this.coords.add(new Point(1,1));
+			this.initCoords(4);
+			this.coords.get(0).add(new Point(-1,0));
+			this.coords.get(0).add(new Point(0,0));
+			this.coords.get(0).add(new Point(1,0));
+			this.coords.get(0).add(new Point(1,1));
+			
+			this.coords.get(1).add(new Point(0,-1));
+			this.coords.get(1).add(new Point(0,0));
+			this.coords.get(1).add(new Point(0,1));
+			this.coords.get(1).add(new Point(-1,1));
+			
+			this.coords.get(2).add(new Point(-1,-1));
+			this.coords.get(2).add(new Point(-1,0));
+			this.coords.get(2).add(new Point(0,0));
+			this.coords.get(2).add(new Point(1,0));
+			
+			this.coords.get(3).add(new Point(0,-1));
+			this.coords.get(3).add(new Point(1,-1));
+			this.coords.get(3).add(new Point(0,0));
+			this.coords.get(3).add(new Point(0,1));
 			break;
 		case 4:
-			this.coords.add(new Point(-1,1));
-			this.coords.add(new Point(-1,0));
-			this.coords.add(new Point(0,0));
-			this.coords.add(new Point(1,0));
+			this.initCoords(4);
+			this.coords.get(0).add(new Point(-1,1));
+			this.coords.get(0).add(new Point(-1,0));
+			this.coords.get(0).add(new Point(0,0));
+			this.coords.get(0).add(new Point(1,0));
+			
+			this.coords.get(1).add(new Point(0,1));
+			this.coords.get(1).add(new Point(0,0));
+			this.coords.get(1).add(new Point(0,-1));
+			this.coords.get(1).add(new Point(-1,-1));
+			
+			this.coords.get(2).add(new Point(-1,0));
+			this.coords.get(2).add(new Point(0,0));
+			this.coords.get(2).add(new Point(1,0));
+			this.coords.get(2).add(new Point(1,-1));
+			
+			this.coords.get(3).add(new Point(0,-1));
+			this.coords.get(3).add(new Point(0,0));
+			this.coords.get(3).add(new Point(0,1));
+			this.coords.get(3).add(new Point(1,1));
 			break;
 		case 5:
-			this.coords.add(new Point(-1,1));
-			this.coords.add(new Point(0,1));
-			this.coords.add(new Point(0,0));
-			this.coords.add(new Point(1,0));
+			this.initCoords(2);
+			this.coords.get(0).add(new Point(-1,1));
+			this.coords.get(0).add(new Point(0,1));
+			this.coords.get(0).add(new Point(0,0));
+			this.coords.get(0).add(new Point(1,0));
+			
+			this.coords.get(1).add(new Point(0,-1));
+			this.coords.get(1).add(new Point(0,0));
+			this.coords.get(1).add(new Point(1,0));
+			this.coords.get(1).add(new Point(1,1));
 			break;
 		case 6:
-			this.coords.add(new Point(-1,0));
-			this.coords.add(new Point(0,0));
-			this.coords.add(new Point(0,1));
-			this.coords.add(new Point(1,0));
+			this.initCoords(4);
+			this.coords.get(0).add(new Point(-1,0));
+			this.coords.get(0).add(new Point(0,0));
+			this.coords.get(0).add(new Point(0,1));
+			this.coords.get(0).add(new Point(1,0));
+			
+			this.coords.get(1).add(new Point(0,-1));
+			this.coords.get(1).add(new Point(0,0));
+			this.coords.get(1).add(new Point(-1,0));
+			this.coords.get(1).add(new Point(0,1));
+			
+			this.coords.get(2).add(new Point(0,-1));
+			this.coords.get(2).add(new Point(-1,0));
+			this.coords.get(2).add(new Point(0,0));
+			this.coords.get(2).add(new Point(1,0));
+			
+			this.coords.get(3).add(new Point(0,-1));
+			this.coords.get(3).add(new Point(1,0));
+			this.coords.get(3).add(new Point(0,0));
+			this.coords.get(3).add(new Point(0,1));
 			break;
 		case 7:
-			this.coords.add(new Point(-1,0));
-			this.coords.add(new Point(0,0));
-			this.coords.add(new Point(0,1));
-			this.coords.add(new Point(1,1));
+			this.initCoords(2);
+			this.coords.get(0).add(new Point(-1,0));
+			this.coords.get(0).add(new Point(0,0));
+			this.coords.get(0).add(new Point(0,1));
+			this.coords.get(0).add(new Point(1,1));
+			
+			this.coords.get(1).add(new Point(0,0));
+			this.coords.get(1).add(new Point(1,0));
+			this.coords.get(1).add(new Point(1,-1));
+			this.coords.get(1).add(new Point(0,1));
 			break;
 		default:
 			break;
+		}
+	}
+	
+	public void changeRotation() {
+		if (this.rotation < this.coords.size() - 1) {
+			this.rotation++;
+		} else {
+			this.rotation = 0;
+		}
+	}
+	
+	public int nextRotation() {
+		if (this.rotation < this.coords.size() - 1) {
+			return this.rotation + 1;
+		} else {
+			return 0;
 		}
 	}
 	
@@ -78,8 +164,18 @@ public class Shape {
 		return this.shape;
 	}
 	
-	public ArrayList<Point> getCoords() {
-		return this.coords;
+	public ArrayList<Point> getCoords(int rotation) {
+		ArrayList<Point> returnedCoords = new ArrayList<>();
+		for(Point p : this.coords.get(rotation)) {
+			returnedCoords.add(p);
+		}
+		return returnedCoords;
+	}
+	
+	private void initCoords(int size) {
+		for (int i = 0; i < size; i++) {
+			this.coords.add(new ArrayList<Point>());
+		}
 	}
 	
 	public void setX(int x) {
@@ -89,6 +185,11 @@ public class Shape {
 	public void setY(int y) {
 		this.y = y;
 	}
+	
+	public int getRotation() {
+		return this.rotation;
+	}
+	
 	
 	
 
